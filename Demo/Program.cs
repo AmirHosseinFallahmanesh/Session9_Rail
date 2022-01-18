ecffe88e356c6c64bc71a6e7ac1f7a018c6f715e
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2022-01-18 11:03:24 +0330

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+    interface IEmailSender
+    {
+        void SendMail(string subject, string content, string destination);
+    }
+
+
+    public class YahooMailSender : IEmailSender
+    {
+        public void SendMail(string subject, string content, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GoogleMailSender : IEmailSender
+    {
+        public void SendMail(string subject, string content, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
